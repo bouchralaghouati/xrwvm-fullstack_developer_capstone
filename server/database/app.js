ad4e250a9abddb5f183cdaa1ae5fbc8896c2b3b0
Bouchra LAGHOUATI <laghouatibouchra@hotmail.com>
2024-12-02 13:35:40 -0500

    module 3 part 1

--- server/database/app.js
+++ server/database/app.js
@@ -58,17 +58,42 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+  try {
+    const documents = await Dealerships.find();
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+    const { state } = req.params;
+    try {
+        const dealers = await Dealerships.find({ state: state });
+        if (dealers.length === 0) {
+            return res.status(404).json({ message: 'No dealers found in this state' });
+        }
+        res.status(200).json(dealers);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to fetch dealerships by state' });
+    }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+    const { id } = req.params;
+    try {
+        const dealer = await Dealerships.findOne({ id });
+        if (!dealer) {
+            return res.status(404).json({ message: 'Dealer not found' });
+        }
+        res.status(200).json(dealer);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to fetch the dealer' });
+    }
 });
 
 //Express route to insert review
